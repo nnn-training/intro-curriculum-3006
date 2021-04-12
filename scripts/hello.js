6a27a72df7adc2074128a5880415172fed23224a
Shun8 <shun.code@gmail.com>
2021-04-12 23:51:33 +0900

    おみくじ機能、username出力するよう修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -3,8 +3,7 @@ module.exports = robot => {
   robot.hear(/おみくじ/i, msg => {
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
     const lot = lots[Math.floor(Math.random() * lots.length)];
-    
-    const user_id = msg.message.user.id;
-    msg.send(`${lot}, <@${user_id}>`);
+    const username = msg.message.user.name;
+    msg.send(`${lot}, ${username}`);
   });
 };
