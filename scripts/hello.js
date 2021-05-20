fa21e972d882996985f8902271cee99f555784c9
suis <n.high.prgrmmr@gmail.com>
2021-05-20 17:10:43 +0900

    アレンジ練習問題: 今日のラッキーハンマー

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+    const user_name = msg.message.user.name;
+    const lots = ['虎丸', '王牙', 'おきつなみ', 'フラカン', 'グーグーベア', '常闇',];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`Hello, <@${user_name}>. Today lucky hammer is ${lot}.`);
   });
 };
