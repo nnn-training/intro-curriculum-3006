9cdb331725bf1932c511de96ff938a80ee6e455b
AABB-OBB <onlylinetibisou@gmail.com>
2021-06-14 15:34:49 +0900

    おみくじ機能の実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
 module.exports = robot => {
-  robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
-  });
+	robot.hear(/hello>/i, msg => {
+		const user_id = msg.message.user.id;
+		msg.send(`Hello, <@${user_id}>`);
+	  });
+	robot.hear('おみくじ', msg => {
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		const userName = msg.message.user.name;
+		msg.send(`${lot}, ${userName}`);
+	});
 };
