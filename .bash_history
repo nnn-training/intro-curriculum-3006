dd0853f4bc2a26247aea677e096eec58d55cd7e1
HasegawaYoshino <hsgw.maron0714@gmail.com>
2021-07-08 14:54:51 +0900

    おみくじのbotを実装

--- .bash_history
+++ .bash_history
@@ -3,3 +3,8 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+yarn install
+exit
+env HUBOT_SLACK_TOKEN=xoxb-2233429329063-2258527327284-kMPJiZ6p2I4zitWKAyT0XGCN bin/hubot --adapter slack
+env HUBOT_SLACK_TOKEN=xoxb-2233429329063-2258527327284-kMPJiZ6p2I4zitWKAyT0XGCN bin/hubot --adapter slack
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,18 @@ module.exports = robot => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+
+  robot.hear(/おみくじ/i, msg => {
+    const username = msg.message.user.name;
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, ${username}`);
+  });
+
+  robot.hear(/ラッキーアイテム/i, msg => {
+    const username = msg.message.user.name;
+    const lots = ['花柄のハンカチ', '黄色いペン', '折りたたみ傘', 'タブレット', 'テッシュ'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, ${username}`);
+  });
 };
