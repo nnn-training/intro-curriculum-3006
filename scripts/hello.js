f08680021632ac30f339a1bde3eeba917459bce8
dd <72111001+nrt0@users.noreply.github.com>
2021-08-08 20:39:37 +0900

    Update hello.js

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
-  });
+        const user_id = msg.message.user.id;
+        msg.send('Hello, ' + username);
+    });
+    robot.hear(/おみくじ/i, msg => {
+        const username = msg.message.user.name;
+        const lots = ['大吉','吉','中吉','末吉','凶'];
+        const lot = lost[Math.floor(Math.random() * lots.length)];
+        msg.send(`${lot}, ${username}`);
+    });
 };
