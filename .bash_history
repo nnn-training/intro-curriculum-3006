c7b87ae0d9bc025e2f9a62872b821a6ff4375348
risarisato <nqg44226@gmail.com>
2021-09-06 00:21:36 +0900

    slackにおみくじ機能を追加しました

--- .bash_history
+++ .bash_history
@@ -3,3 +3,32 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+yarn install
+bin\hubot
+bin/hubot
+cd ..
+ls
+ls
+exit
+pwd
+cd ..
+ls
+cd ..
+ls
+ls
+cd ~
+ls
+cd scripts/
+ls
+bin/hubot
+cd ~
+ls
+exit
+bin/hubot
+bin/hubot
+bin/hubot
+exit
+ls
+exit
+env HUBOT_SLACK_TOKEN=xoxb-1564865243874-2457556748274-s8MpMUutQCmYddiIFAKBnyaG bin/hubot --adapter slack
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,18 @@
+// 厳格モードは詳細エラーとか警告文を出す
 'use strict';
+// module.exportsは別ファイルからrobotが呼び出す
 module.exports = robot => {
+    // 正規表現でhelloと発言したら「i」は小文字大文字関係なく
   robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+      // ユーザIDを取る
+    const username = msg.message.user.name;
+    // helloと発言があったら、ユーザID、helloを返す
+    msg.send('Hello, ' + username);
   });
+robot.hear(/おみくじ/i, msg => {
+  const username = msg.message.user.name;
+  const lots = ['大好き','好き','1人でいい','かまってちゃん','メンヘラちゃん'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  msg.send(`${lot}, ${username}`);
+});
 };
