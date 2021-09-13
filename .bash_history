75a0587d8f300cd714f91a60cc9d0ecafb38e2bd
Happy Kumamoto <zen.word+hub@gmail.com>
2021-09-14 01:47:13 +0900

    「きょうのおやつ」を実装

--- .bash_history
+++ .bash_history
@@ -3,3 +3,9 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+yarn install
+bin/hubot
+env HUBOT_SLACK_TOKEN=xoxb-1362976486966-2490959932580-U0mb6Paw8GzRCtiInPwJg3ea bin/hubot --adapter slack
+env HUBOT_SLACK_TOKEN=xoxb-1362976486966-2490959932580-U0mb6Paw8GzRCtiInPwJg3ea bin/hubot --adapter slack
+env HUBOT_SLACK_TOKEN=xoxb-1362976486966-2490959932580-U0mb6Paw8GzRCtiInPwJg3ea bin/hubot --adapter slack
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,11 @@ module.exports = robot => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+  robot.hear(/おやつは？/i, msg => {
+    const username = msg.message.user.name;
+    const lots = ['🍮Custard', '🥧Pie', '🍨 Ice Cream', '🍪Cookie', 
+    '🍰Shortcake', '🍡三色だんご', '🧁Cupcake', '🧇Waffle','🥮月餅'];
+    const lot = lots[Math.floor(Math.random()*lots.length)];
+    msg.send(`きょうのおやつは、${lot}です！ (o^−^o) , ${username}`);
+  });
 };
