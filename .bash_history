cd60bc703c57f16fd5f51a5d8912c23f81512752
Ryoma Nozaki <maronteena@ozzio.jp>
2021-09-14 19:19:12 +0900

    おみくじ

--- .bash_history
+++ .bash_history
@@ -3,3 +3,10 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+yarn install
+exit
+code .
+code .
+exit
+git commit -am "おみくじ"
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -5,3 +5,9 @@ module.exports = robot => {
     msg.send(`Hello, <@${user_id}>`);
   });
 };
+ robot.hear(/おみくじ>/i, msg => {
+   const username = msg.message.user.name;
+   const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+   const lot = lots[Math.floor(Math.random() * lots.lenght)];
+   msg.send(`${lot}, ${username}`);
+ });
