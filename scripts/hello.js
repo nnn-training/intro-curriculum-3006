e4f3eb110c06dee9c894a728d1a992a342d19f0b
pzw05mux <pzw05mux@s.okayama-u.ac.jp>
2021-09-14 16:09:48 +0900

    helloコマンドを間違えて消していたので修正

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,5 +1,10 @@
 'use strict';
 module.exports = (robot) => {
+  robot.hear(/hello>/i, (msg) => {
+    const username = msg.message.user.name;
+    msg.send('Hello, ' + username);
+  });
+
   robot.hear(/おみくじ/, (msg) => {
     const user_id = msg.message.user.id;
     const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
