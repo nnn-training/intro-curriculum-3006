5ff0ca17f307acaf3e51aad17dd42a11acf23c0d
Fuuchin <denkisutandokese@outlook.jp>
2021-09-23 16:21:53 +0900

    おみくじです。面白いですよ。

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,22 @@
 'use strict';
 module.exports = robot => {
-  robot.hear(/hello>/i, msg => {
-    const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+  robot.hear(/おみくじしたい！/i, msg => {
+    const lots = ['おめでとう！大吉', '吉', '中吉', '末吉', '残念！凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    const username = msg.message.user.name;
+    msg.send(`いいよ！ジャジャーン！${username}さんは、${lot}です！`);
+  });
+
+  // こわい言い方
+  robot.hear(/おみくじやらさせてくれよな！ちなみに凶やったらしばくぞ/i, msg => {
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    const username = msg.message.user.name;
+    msg.send(`へへぇ${username}さんは、${lot}ですぅぅ`);
+  });
+
+  robot.hear(/おいなんで凶なんや？どうゆうこと？あ？/i, msg => {
+    const username = msg.message.user.name;
+    msg.send(`へへぇすみません${username}さんは、大吉ですぅぅ`);
   });
 };
