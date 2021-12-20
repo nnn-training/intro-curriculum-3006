4c646435a44bba5317c5de6e186c3af352802cd7
r-40021 <75155258+r-40021@users.noreply.github.com>
2021-12-20 22:08:38 +0900

    おみくじ機能を追加

--- .bash_history
+++ .bash_history
@@ -3,3 +3,13 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+exit
+yarn install
+bi/hubot
+bin/hubot
+bin/hubot
+ls
+bin/hubot
+./bin/hubot
+/bin/hubot
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,16 @@
 'use strict';
+
+const { loadBot } = require("hubot/es2015");
+
 module.exports = robot => {
   robot.hear(/hello>/i, msg => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+  robot.hear(/おみくじ/i, msg => {
+    const user_id = msg.message.user.id;
+    const lots = ['大吉','吉','中吉','末吉','凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
+  })
 };
