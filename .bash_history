92b74b286fa086aa9d38eb8eab8d4882278f5f13
米田祐至 <yujiyoneda@yonedayuuitarinoMac-mini.local>
2022-01-12 15:30:31 +0900

    おみくじ機能の実装

--- .bash_history
+++ .bash_history
@@ -3,3 +3,27 @@ yo hubot-yarn
 git status
 bin/hubot
 exit
+ls
+scripts/hello.js
+yarn install
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+bin/hubot
+docker-compose ps -a
+ls
+git add .
+git status
+git commit -m "おみくじ機能実装"
+git status
+git push origin main-2021
+git status
+ls
+git push origin main-2021
+git branch
+docker-compose down
+exit
--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = robot => {
-  robot.hear(/hello>/i, msg => {
+  robot.hear(/おみくじ/i, msg => {
     const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+    const user_name = msg.message.user.name;
+    const lots =['大吉', '中吉', '小吉', '吉', '凶', '大凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`@${user_name}さんのおみくじの結果は ${lot} でした。 <@${user_id}>`);    
   });
-};
+}
