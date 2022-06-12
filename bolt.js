ea8862385db41a77a6c6260deca0c1037306f41a
miki-same <107123221+miki-same@users.noreply.github.com>
2022-06-12 17:30:14 +0900

    フォーマットの整形

--- bolt.js
+++ bolt.js
@@ -6,26 +6,26 @@ dotenv.config();
 console.log(`SLACK_APP_TOKEN: ${process.env.SLACK_APP_TOKEN}`);
 console.log(`SLACK_BOT_TOKEN: ${process.env.SLACK_BOT_TOKEN}`);
 
-function omikuji(){
-    const lots=['大吉', '吉', '中吉', '末吉', '凶'];
-    const lot = lots[Math.floor(Math.random()*lots.length)];
-    return lot;
+function omikuji() {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  return lot;
 }
 
 const app = new bolt.App({
-    token: process.env.SLACK_BOT_TOKEN,
-    appToken: process.env.SLACK_APP_TOKEN,
-    socketMode: true,
-    logLevel: 'debug'
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+  logLevel: 'debug'
 });
 
-app.message(/hello/i, ({message,say})=>{
-    say(`こんにちは！ <@${message.user}>さん`);
+app.message(/hello/i, ({ message, say }) => {
+  say(`こんにちは！ <@${message.user}>さん`);
 });
 
-app.message('おみくじ', ({message, say})=>{
-    const lot=omikuji();
-    say(`${lot}, <@${message.user}>`);
+app.message('おみくじ', ({ message, say }) => {
+  const lot = omikuji();
+  say(`${lot}, <@${message.user}>`);
 })
 
 app.start();
