3c21ec2cbe4e00ffd400edce0f9e3d53146ecc1d
maiamea <maiameawork@gmail.com>
2022-08-08 14:18:23 +0900

    おみくじの処理を追加

--- scripts/hello.js
+++ scripts/hello.js
@@ -4,4 +4,17 @@ module.exports = robot => {
     const user_id = msg.message.user.id;
     msg.send(`Hello, <@${user_id}>`);
   });
+  robot.hear(/omikuji2021/i, msg => {
+    const user_id = msg.message.user.id;
+    const result = omikuji();
+    msg.send(`${result}, <@${user_id}>`);
+  });
 };
+
+
+function omikuji() {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  let randomIndex = Math.floor(Math.random() * lots.length);
+  let lot = lots[randomIndex];
+  return lot;
+}
