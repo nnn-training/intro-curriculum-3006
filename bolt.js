af12e0d056a8e5c0d7bd5f155ce6105d85e28407
Fuuchin <denkisutandokese@outlook.jp>
2022-09-13 19:46:40 +0900

    おみくじボット作った～

--- bolt.js
+++ bolt.js
@@ -11,7 +11,13 @@ const app = new bolt.App({
 });
 
 app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+  say(`こんにちは！ <@${message.user}>さん`);
+});
+
+app.message('おみくじ', ({message, say}) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶', '現段階では大凶ですが、「N予備校は最高」と443回唱えたら大大大吉まで上がります'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`<@${message.user}>さんは「${lot}」です！`)
 });
 
 app.start();
