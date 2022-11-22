e0736b2ea095518eecac1386544df5bed7bcf7b3
akazawa <ak.git.acc@outlook.jp>
2022-11-22 14:16:39 +0900

    おみくじの実装

--- bolt.js
+++ bolt.js
@@ -1,6 +1,6 @@
 'use strict';
 const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = new bolt.App({
@@ -10,8 +10,17 @@ const app = new bolt.App({
   logLevel: 'debug'
 });
 
-app.message(/hello/i, ({message, say}) => {
+app.message(/hello/i, ({ message, say }) => {
   say('こんにちは！');
+});
+
+// const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+// const lot = lots[Math.floor(Math.random() * lots.length)];
+
+app.message('おみくじ', ({ message, say }) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);
 });
 
 app.start();
