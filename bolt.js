943d1e2be860062d5493554ecee0211e657d1d4f
fool-hy <gmirionman@gmail.com>
2023-02-06 21:34:21 +0900

    練習問題

--- bolt.js
+++ bolt.js
@@ -10,8 +10,15 @@ const app = new bolt.App({
   logLevel: 'debug'
 });
 
-app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+// app.message(/おみくじ/i, ({message, lot}) => {//lotの宣言が被っている。実行するための引数
+//   const lots = ['大吉','吉','中吉','末吉','凶'];
+//   const lot = lots[Math.rondom() * lots.length];
+// });
+
+app.message(/おみくじ/i, ({message, say}) => {
+  const lots = ['大吉','吉','中吉','末吉','凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);//自分にメンションさせる
 });
 
 app.start();
