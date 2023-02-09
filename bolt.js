40b99fc86bb9de82f7445b39b7a3b6b2826a6d00
akanekwon172 <akanekwon_dev@icloud.com>
2023-02-10 00:06:39 +0900

    おみくじの実装

--- bolt.js
+++ bolt.js
@@ -1,17 +1,28 @@
 'use strict';
 const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
-  logLevel: 'debug'
+  logLevel: 'debug',
 });
 
-app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+message(/hello/i, async ({ message, say }) => {
+  await say(`こんにちは！  <@${message.user}>さん！`);
 });
+
+message('おみくじ', async ({ message, say }) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+
+  await say(`${lot}, <@${message.user}>`);
+});
+
+async function message(pattern, fn) {
+  await app.message(pattern, fn);
+}
 
 app.start();
