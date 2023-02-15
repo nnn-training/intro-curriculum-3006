95d17d17ecc59043604f6b72829cb623905c3367
warabimochi <ecccompsenyou@gmail.com>
2023-02-15 17:47:56 +0900

    おみくじ機能の追加

--- bolt.js
+++ bolt.js
@@ -14,4 +14,10 @@ app.message(/hello/i, ({message, say}) => {
   say('こんにちは！');
 });
 
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+app.message('おみくじ', ({message, say}) => {
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);
+});
+
 app.start();
