2013687723bcdfc6fff4a836b3b00cd049ccb74b
chambear <chambear@my-macbook.local>
2023-08-23 23:01:38 +0900

    結果が同じにならないように改修

--- bolt.js
+++ bolt.js
@@ -1,8 +1,6 @@
 'use strict';
 const bolt = require('@slack/bolt');
 const dotenv = require('dotenv');
-const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
-const lot = lots[Math.floor(Math.random() * lots.length)];
 
 dotenv.config();
 
@@ -17,7 +15,9 @@ app.message(/hello/i, ({ message, say }) => {
   say('こんにちは！');
 });
 
-app.message('おみくじ', ({ message, say})=>{
+app.message('おみくじ', ({ message, say }) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
   say(`${lot},<@${message.user}>`);
 })
 
