96027f046274009cdd6c172e7ff806d38edd5639
chambear <chambear@my-macbook.local>
2023-08-23 22:58:34 +0900

    おみくじ機能の実装

--- bolt.js
+++ bolt.js
@@ -1,6 +1,9 @@
 'use strict';
 const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+const lot = lots[Math.floor(Math.random() * lots.length)];
+
 dotenv.config();
 
 const app = new bolt.App({
@@ -10,8 +13,12 @@ const app = new bolt.App({
   logLevel: 'debug'
 });
 
-app.message(/hello/i, ({message, say}) => {
+app.message(/hello/i, ({ message, say }) => {
   say('こんにちは！');
 });
+
+app.message('おみくじ', ({ message, say})=>{
+  say(`${lot},<@${message.user}>`);
+})
 
 app.start();
