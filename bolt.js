919d3dbbd0ca394763656b85b866f20365340fac
Yamamoto Ren <ren.yamamoto@azone-japan.co.jp>
2023-10-21 23:09:03 +0900

    おみくじ実装

--- bolt.js
+++ bolt.js
@@ -1,17 +1,19 @@
-'use strict';
-const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+"use strict";
+const bolt = require("@slack/bolt");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
-  logLevel: 'debug'
+  logLevel: "debug",
 });
 
-app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+app.message(/おみくじ/i, ({ message, say }) => {
+  const lots = ["大吉", "吉", "中吉", "末吉", "凶"];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);
 });
 
 app.start();
