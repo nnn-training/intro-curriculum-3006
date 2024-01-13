79c9107ed6e5486b9898353be9162be24514f5c1
sagawa <sagasyunta@gmail.com>
2024-01-13 16:52:02 +0900

    おみくじ実装

--- bolt.js
+++ bolt.js
@@ -3,6 +3,7 @@ const bolt = require('@slack/bolt');
 const dotenv = require ('dotenv');
 dotenv.config();
 
+
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
@@ -12,6 +13,12 @@ const app = new bolt.App({
 
 app.message(/hello/i, ({message, say}) => {
   say('こんにちは！');
+});
+
+app.message('おみくじ', ({message, say}) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);
 });
 
 app.start();
